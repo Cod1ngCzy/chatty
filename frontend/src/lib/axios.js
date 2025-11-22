d4dfceefac6ef251a5e0f72abe0edb1d9107bd11
czypi <codingguru.czy@gmail.com>
2025-11-22 21:48:11 +0800

    Fixed url for deployment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://192.168.5.136:5001/api/v1" : "/api",
+    baseURL: import.meta.env.MODE === "development" ? "http://192.168.5.136:5001/api/v1" : "/api/v1",
     withCredentials: true,
 });
